befc8e5023ec47241ce267831d852f82c3728f73
Eric Christiansen <ericmartinchristiansen@gmail.com>
2013-02-26 11:17:24 -0800

    added meta include

---
+++ src/salve/salve.hpp
@@ -0,0 +1,12 @@
+#ifndef SALVE_HPP_
+#define SALVE_HPP_
+
+#include "CanBuildFrom.hpp"
+#include "collections.hpp"
+#include "Comparable.hpp"
+#include "Contains.hpp"
+#include "Functor.hpp"
+#include "Seq.hpp"
+#include "Showable.hpp"
+
+#endif /* SALVE_HPP_ */
--- src/testFunctor.cpp
+++ src/testFunctor.cpp
@@ -27,3 +27,28 @@ TEST(TestFunctor, VectorInt) {
 
   EXPECT_TRUE(equals(wrapped, golden));
 }
+
+template<typename A, typename Func>
+auto map_(const std::vector<A>& orig,
+          Func f) -> std::vector<decltype(f(orig[0]))> {
+  std::vector<decltype(f(orig[0]))> rv;
+  rv.reserve(orig.size());
+  for (const A& a : orig) {
+    rv.push_back(f(a));
+  }
+  return rv;
+}
+
+TEST(TestFunctor, VectorIntLambdaFunction) {
+  const vector<int> ints = { 1, 2, 3 };
+
+  const vector<int> out = map_(ints, [](int x) {return x + 1;});
+
+  const vector<int> golden = { 2, 3, 4 };
+  EXPECT_TRUE(equals(out, golden));
+
+  // TODO: This next line should compile (shouldn't need to provide template
+  // parameters or cast the lambda to a function). See:
+  // http://stackoverflow.com/questions/7950680/how-can-i-pass-a-lambda-c11-into-a-templated-function
+//  fmap([](int x){ return x + 1; }, ints);
+}
